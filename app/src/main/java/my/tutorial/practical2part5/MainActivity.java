bc13414df093f790db0277bc03e9c1dc3fa92826
Heja Bibani <hejabibani@live.com>
2022-08-01 17:11:39 +1000

    Update

--- app/src/main/java/my/tutorial/practical2part5/MainActivity.java
+++ app/src/main/java/my/tutorial/practical2part5/MainActivity.java
@@ -45,7 +45,7 @@ public class MainActivity extends AppCompatActivity {
 
             }
             Double areaOfCircle = Math.PI * radius * radius;
-            msg.setText(areaOfCircle.toString() + "cm^2");
+            msg.setText(areaOfCircle.toString() + " cm\u00B2");
         }
 
 
